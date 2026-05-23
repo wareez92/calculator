b05ceb6825c2252dcd2eaede68eece0a21076b1b
wareez <wareez92@gmail.com>
2026-05-23 12:08:09 -0500

    Box-sizing set to border-box

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,38 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      Hello World!2
-    </>
+        <div className="calculator-grid">
+
+              {/* ---Output--- */}
+
+              <div className='output'>
+                  <div className='previous-operand'></div>
+                  <div className='current-opperand'></div>
+              </div>
+
+              {/* ---Buttons--- */}
+
+              <button className='span-two'>AC</button>
+              <button>DEL</button>
+              <button>÷</button>
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>*</button>
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+              <button>+</button>
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+              <button>-</button>
+              <button>.</button>
+              <button>0</button>
+              <button className='span-two'>=</button>
+        </div>
   )
 }
 

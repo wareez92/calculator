a5c8c8f2a8dff05f880fd52582a47f66772d664c
wareez <wareez92@gmail.com>
2026-06-05 18:35:05 -0500

    currentOperand set

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,64 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function reducer(state, action) {
+const ACTIONS = {
+      ADD_DIGIT: 'add-digit',
+      CHOOSE_OPERATION: 'choose-operation',
+      CLEAR: 'clear',
+      DELETE_DIGIT: 'delete-digit',
+      EVALUTE: 'evaluate'
+}
 
+function reducer(state, { type, payload }) {
+      switch (type) {
+            case ACTIONS.ADD_DIGIT:
+                  return {
+                        ...state,
+                        currentOperand: '${currentOperand || ""}${payload.digit}'
+                  }
+      }
 }
 
 function App() {
 
-      const [{ currentOperand, previousOperand, Operation}, dispatch] = useReducer(reducer,{})
+      const [{ currentOperand, previousOperand, Operation }, dispatch] = useReducer(reducer, {})
 
-// Calculator Interface
+      dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } })
 
-  return (
-        <div className="calculator-grid">
+      // ---Calculator Interface--- //
 
-              {/* ---Output--- */}
+      return (
+            <div className="calculator-grid">
 
-              <div className='output'>
-                  <div className='previous-operand'></div>
-                  <div className='current-operand'></div>
-              </div>
+                  {/* ---Output--- */}
 
-              {/* ---Buttons--- */}
+                  <div className='output'>
+                        <div className='previous-operand'></div>
+                        <div className='current-operand'></div>
+                  </div>
 
-              <button className='span-two'>AC</button>
-              <button>DEL</button>
-              <button>÷</button>
-              <button>1</button>
-              <button>2</button>
-              <button>3</button>
-              <button>*</button>
-              <button>4</button>
-              <button>5</button>
-              <button>6</button>
-              <button>+</button>
-              <button>7</button>
-              <button>8</button>
-              <button>9</button>
-              <button>-</button>
-              <button>.</button>
-              <button>0</button>
-              <button className='span-two'>=</button>
-        </div>
-  )
+                  {/* ---Buttons--- */}
+
+                  <button className='span-two'>AC</button>
+                  <button>DEL</button>
+                  <button>÷</button>
+                  <button>1</button>
+                  <button>2</button>
+                  <button>3</button>
+                  <button>*</button>
+                  <button>4</button>
+                  <button>5</button>
+                  <button>6</button>
+                  <button>+</button>
+                  <button>7</button>
+                  <button>8</button>
+                  <button>9</button>
+                  <button>-</button>
+                  <button>.</button>
+                  <button>0</button>
+                  <button className='span-two'>=</button>
+            </div>
+      )
 }
 
 export default App

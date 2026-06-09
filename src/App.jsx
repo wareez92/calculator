98834e4d6cc91870a542c39d914266144912b628
wareez <wareez92@gmail.com>
2026-06-09 18:38:52 -0500

    DigiButton component implemented

--- src/App.jsx
+++ src/App.jsx
@@ -46,20 +46,20 @@ function App() {
                   <button className='span-two'>AC</button>
                   <button>DEL</button>
                   <DigitButton digit="÷" dispatch={dispatch} />
-                  <button>1</button>
-                  <button>2</button>
-                  <button>3</button>
+                  <DigitButton digit="1" dispatch={dispatch}/>
+                  <DigitButton digit="2" dispatch={dispatch}/>
+                  <DigitButton digit="3" dispatch={dispatch}/>
                   <button>*</button>
-                  <button>4</button>
-                  <button>5</button>
-                  <button>6</button>
+                  <DigitButton digit="4" dispatch={dispatch}/>
+                  <DigitButton digit="5" dispatch={dispatch}/>
+                  <DigitButton digit="6" dispatch={dispatch}/>
                   <button>+</button>
-                  <button>7</button>
-                  <button>8</button>
-                  <button>9</button>
+                  <DigitButton digit="7" dispatch={dispatch}/>
+                  <DigitButton digit="8" dispatch={dispatch}/>
+                  <DigitButton digit="9" dispatch={dispatch}/>
                   <button>-</button>
-                  <button>.</button>
-                  <button>0</button>
+                  <DigitButton digit="." dispatch={dispatch}/>
+                  <DigitButton digit="0" dispatch={dispatch}/>
                   <button className='span-two'>=</button>
             </div>
       )
